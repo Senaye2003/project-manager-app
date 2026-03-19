868cd4145c8b944fda5d5ab78567081bd33fe947
senaye <sweldebe@charlotte.edu>
2026-03-18 23:50:37 -0400

    implement tasks page with data fetching and update functionality

--- project-manager-frontend/src/pages/Tasks.jsx
+++ project-manager-frontend/src/pages/Tasks.jsx
@@ -1,9 +1,51 @@
-import { getMyTasks, updateTask } from "../api/tasks"
+import { useEffect, useState } from "react";
+import { getMyTasks, updateTask } from "../api/tasks";
 
-export function Tasks(){
-    return (
-        <>
-            
-        </>
-    )
-}j
+
+export function Tasks() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+     const fetchData = async() => {
+        const tasksSet = await getMyTasks()
+        setTasks(tasksSet)
+    }
+    fetchData()
+  }, [])
+
+  async function handleUpdate(id){
+
+    try {
+        const updates = {
+            status: "IN_PROGRESS",
+        };
+
+        const updatedTask = await updateTask(id, updates)
+        
+        const updatedTasks = tasks.map((task) => (
+            task.id === id ? updatedTask : task
+        ))
+
+        setTasks(updatedTasks)
+
+    } catch (error) {
+        console.error("update failed", error);
+    }
+
+  }
+  return (
+    <>
+      {tasks.map((task)=> (
+        <div key={task.id}>
+           <p > {task.title} </p>
+           <p > {task.status} </p>
+           <button onClick={() => handleUpdate(task.id)}> Update </button>
+        </div>
+        
+      ))}
+    </>
+  )
+
+  ;
+}
+
