370b0b3b2afb9c3e38dfd27449378e0c3faa60b1
senaye <sweldebe@charlotte.edu>
2026-04-01 14:55:11 -0400

    implemented a nav bar page

--- project-manager-frontend/src/App.jsx
+++ project-manager-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Route, Routes, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { Login } from "./components/Login";
 import { ProtectedRoute } from "./layouts/ProtectedRoute";
@@ -10,17 +11,32 @@ import { Teams } from "./pages/Teams";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+    <div className="app">
+      <div className="navbar">
+        <h3>Project Manager</h3>
 
-      <Route element={<ProtectedRoute />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/teams" element={<Teams />} />
-      </Route>
-    </Routes>
+        <div className="navbar__links">
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/tasks">Tasks</Link>
+          <Link to="/teams">Teams</Link>
+        </div>
+      </div>
+
+      <div className="container">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/teams" element={<Teams />} />
+          </Route>
+        </Routes>
+      </div>
+    </div>
   );
 }
 
